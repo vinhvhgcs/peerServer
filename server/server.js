af9ab625841309a47946221bce10c4bcafd5998f
Vinh Vo <voh.vinh803@gmail.com>
2022-11-28 22:28:51 +0700

    on

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { ExpressPeerServer } = require('peer');
 const app = express();
 
+app.get('/', (req, res, next) => res.send('Hello world!'));
+
 const peer = app.listen(9000);
 
 const peerServer = ExpressPeerServer(peer, {
   path: '/'
 });
 
-app.use("/stream", peerServer);
+app.use("/", peerServer);
